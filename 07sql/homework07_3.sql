dad005975ef46958300a85a19cbb3e3d07abf01a
Lydia Cupery <lydiacpry@gmail.com>
2017-03-30 22:57:46 -0400

    delete

--- 07sql/homework07_3.sql
+++
@@ -1,2 +0,0 @@
-a) The query on which your view from exercise 1 is based - Write this query using both the relational algebra and tuple relational calculus, with respect to the original HR relations.
-b) The query from exercise 1.a - Write this query using (only) the relational calculus, with respect to DeptView.
