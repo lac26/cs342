8ee19af4f7b3a57396570018372a8ec847ed59cc
Lydia Cupery <lydiacpry@gmail.com>
2017-04-17 09:05:33 -0400

    documentation for procedures

--- project/procedures.sql
+++ project/procedures.sql
@@ -1,3 +1,16 @@
+/* insert_book_for_person takes in parameters:
+quantp - quantity of book to insert for person
+personp - person to insert the book for
+bookidp - id of book to insert
+bookauthorp - author of book to insert
+booktitlep - title of book to insert
+bookeditionp - edition of book to insert
+bookconditionp - condition of book to insert
+
+if the book already exists, simply update the quantity of the book the person has
+
+if the book does not exist, create an instance of that book with the given quantity for the given person
+*/
 CREATE OR REPLACE PROCEDURE insert_book_for_person (quantp in PersonBook.quant%type, personidp in Person.id%type,
 bookidp in Book.id%type, bookauthorp in Book.author%type, booktitlep in Book.title%type,
 bookeditionp in Book.edition%type, bookconditionp in Book.condition%type)
@@ -19,18 +32,19 @@ INTO personCounter
 FROM Person P
 WHERE personidp = P.id;
 If entryCounter > 0 then
-dbms_output.put_line('already is an instance of this, adjust quantity');
-elsif personCounter > 0 then
+dbms_output.put_line('book already exists so will update quantity of existing book');
+updateQuant(quantp, bookidp, personidp);
+elsif personCounter > 0 and personCounter > 0 then
 		if bookCounter < 1 then
 		dbms_output.put_line('creating book and inserting');
 		INSERT INTO Book VALUES (bookidp, bookauthorp, booktitlep, bookeditionp, bookconditionp);
 		INSERT INTO PersonBook VALUES(bookidp, personidp, quantp);
 		else
-		RAISE_APPLICATION_ERROR(-20000, 'Book ' || bookidp ||
-    	' already exists' );
+		RAISE_APPLICATION_ERROR(-20002, 'Book ' || bookidp ||
+    	' already exists but corresponding person entry doess not.  Please re-try query with different book or different person' );
 		end if;
 else 
-RAISE_APPLICATION_ERROR(-20000, 'Person ' || personidp ||
+RAISE_APPLICATION_ERROR(-20003, 'Person ' || personidp ||
     	' does not exist' );
 END IF;
 END;
@@ -38,11 +52,18 @@ END;
 show errors;
 
 BEGIN 
-insert_book_for_person(1, 1, 100,'Jonas','Kraft',2,'awful');
+insert_book_for_person(5, 1, 100,'Jonas','Kraft',2,'awful');
 END;
 /
 --it works!
 
+/* updateQuant updates the quantity of a certain book a person has
+quantp - new quantity of book belonging to person with personidp
+personidp - the id of the owner of the book
+bookidp - the id of the book
+
+If an entry already exists for the book, update it.  Otherwise, throw an error.
+*/
 CREATE OR REPLACE PROCEDURE updateQuant (quantp in PersonBook.quant%type, bookidp in Book.id%type, personidp in Person.id%type) 
 AS counter integer;
 BEGIN
@@ -58,7 +79,7 @@ elsif counter = 0 then
 	RAISE_APPLICATION_ERROR(-20000, 'Person ' || personidp ||
     	' with book ' || bookidp || ' does not exist.');
 else 
-	RAISE_APPLICATION_ERROR(-20000, 'Person ' || personidp ||
+	RAISE_APPLICATION_ERROR(-20001, 'Person ' || personidp ||
     	' with book ' || bookidp || ' has mutliple records.  Fix this before updating.');
 end if;
 END;
