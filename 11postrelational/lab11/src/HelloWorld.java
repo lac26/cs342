10675bcea6a297d1409936120b1403d69c711fea
Lydia Cupery <lac26@calvin.edu>
2017-04-27 21:29:31 -0400

    homework 11

--- 11postrelational/lab11/src/HelloWorld.java
+++ 11postrelational/lab11/src/HelloWorld.java
@@ -1,16 +1,41 @@
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 
 // The Java class will be hosted at the URI path "/helloworld"
 @Path("/hello")
 public class HelloWorld {
     // The Java method will process HTTP GET requests
+
+    @Path("/api")
     @GET
-    // The Java method will produce content identified by the MIME Media type "text/plain"
     @Produces("text/plain")
-    public String getClichedMessage() {
-        // Return some cliched textual content
-        return "Hello World";
+    public String getMessage() {
+        return "Getting...";
     }
+
+    @Path("/api/{var1}")
+    @PUT
+    @Produces("text/plain")
+    public String doPut(@PathParam("var1") Integer myNum){
+        return "Putting  " + myNum;
+    }
+
+    @Path("/api/{stringVar}")
+    @POST
+    @Produces("text/plain")
+    public String doPost(@PathParam("stringVar") String myString){
+        return "string is " + myString;
+    }
+
+    @Path("/api/{delVar}")
+    @DELETE
+    @Produces("text/plain")
+    public String doDelete(@PathParam("delVar") Integer toDelete){
+        if(toDelete<0 || toDelete>9){
+            return "please return a value between 0 and 9";
+        }
+        else {
+            return "deleting " + toDelete;
+        }
+    }
+
 }
